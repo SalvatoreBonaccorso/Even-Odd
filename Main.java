1e3ae50e7e58e2d92d497ea49ce4b1e54cc261c8
Salvatore Bonaccorso <tury202737@hotmail.it>
2018-10-15 23:13:19 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        EvenOdd f= new EvenOdd ();
+        EvenOdd f2= new EvenOdd ();
+        System.out.println(f.play("pari",3));
+        System.out.println(f2.playWithDivision("dispari",1));
+    }
+}
